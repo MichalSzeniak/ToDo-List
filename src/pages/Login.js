215f1043f067c56d01d487126f30bb1ac4bc719b
Michał Szeniak <michal.szeniak@gmail.com>
2021-07-16 22:13:01 +0200

    feat: Register fetch

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,19 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import '../style/Login.scss'
+import "../style/Login.scss";
 
 const Login = () => {
+
+  const [login, setLogin] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
+
   return (
     <section className="login">
       <div className="login__container">
         <h2 className="login__header">Login</h2>
-        <form className="login__form">
-          <input className="login__input" type="text" placeholder="Email or Username" required />
-          <input className="login__input" type="password" placeholder="Password" required />
+        <form className="login__form" onSubmit={handleSubmit}>
+          <input
+            className="login__input"
+            type="text"
+            placeholder="Email or Username"
+            required
+            value={login}
+            onChange={(e) => setLogin(e.target.value)}
+          />
+          <input
+            className="login__input"
+            type="password"
+            placeholder="Password"
+            required
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
           <button className="login__button">Login</button>
         </form>
         <span className="login__or">or</span>
-        <Link to="/register" className="login__registerLink">Create a account</Link>
+        <Link to="/register" className="login__registerLink">
+          Create a account
+        </Link>
       </div>
     </section>
   );
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,26 +1,84 @@
-import React from 'react';
-import '../style/Register.scss'
-import arrow from '../assets/Arrow.png'
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import "../style/Register.scss";
+import arrow from "../assets/Arrow.png";
+import { Link, useHistory } from "react-router-dom";
 
 const Register = () => {
-    return (
-        <section className="register">
-        <div className="register__container">
-            <Link to="/" className="register__arrowIcon">
-                <img src={arrow} alt=""/>
-            </Link>
-          <h2 className="register__header">Create a new account</h2>
-          <form className="register__form">
-            <input className="register__input" type="text" placeholder="Username" required />
-            <input className="register__input" type="email" placeholder="Email" required />
-            <input className="register__input" type="password" placeholder="Password" required />
-            <input className="register__input" type="password" placeholder="Repeat password" required />
-            <button className="register__button">Create</button>
-          </form>
-        </div>
-      </section>
-    )
-}
+  const [username, setUserName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [rePassword, setRePassword] = useState("");
+  const [error, setError] = useState(null);
+  const history = useHistory();
 
-export default Register
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (password !== rePassword) {
+      alert("Passwords don't match");
+    } else {
+      const data = {username, email, password };
+
+      fetch(`https://recruitment.ultimate.systems/auth/local/register`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(data),
+      })
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data)
+            history.push("/")
+            setError(null)
+        })
+        .catch((err) => setError(err.message))
+
+    }
+  };
+
+  return (
+    <section className="register">
+      <div className="register__container">
+        <Link to="/" className="register__arrowIcon">
+          <img src={arrow} alt="" />
+        </Link>
+        <h2 className="register__header">Create a new account</h2>
+        <form className="register__form" onSubmit={handleSubmit}>
+          <input
+            className="register__input"
+            type="text"
+            placeholder="Username"
+            required
+            value={username}
+            onChange={(e) => setUserName(e.target.value)}
+          />
+          <input
+            className="register__input"
+            type="email"
+            placeholder="Email"
+            required
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            className="register__input"
+            type="password"
+            placeholder="Password"
+            required
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <input
+            className="register__input"
+            type="password"
+            placeholder="Repeat password"
+            required
+            value={rePassword}
+            onChange={(e) => setRePassword(e.target.value)}
+          />
+          <button className="register__button">Create</button>
+        </form>
+      </div>
+    </section>
+  );
+};
+
+export default Register;
